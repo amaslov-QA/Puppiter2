1d74cad924c4acd371426a8ab8f5b4fd244e3d49
aa_maslov <smart.maslov@gmail.com>
2022-04-27 10:21:02 +0300

    V1

--- pageChoiceOfLocation.js
+++ pageChoiceOfLocation.js
@@ -1,15 +1,4 @@
 module.exports = {
-  row1Plase1: "main > section div:nth-child(1) > span:nth-child(1)",
-  row1Plase2: "main > section div:nth-child(1) > span:nth-child(2)",
-  row1Plase3: "main > section div:nth-child(1) > span:nth-child(3)",
-  row1Plase4: "main > section div:nth-child(1) > span:nth-child(4)",
-  row1Plase5: "main > section div:nth-child(1) > span:nth-child(5)",
-  row1Plase6: "main > section div:nth-child(1) > span:nth-child(6)",
-  row1Plase7: "main > section div:nth-child(1) > span:nth-child(7)",
-  row1Plase8: "main > section div:nth-child(1) > span:nth-child(8)",
-  row1Plase9: "main > section div:nth-child(1) > span:nth-child(9)",
-  row1Plase10: "main > section div:nth-child(1) > span:nth-child(10)",
-
   row2Plase1: "main > section div:nth-child(2) > span:nth-child(1)",
   row2Plase2: "main > section div:nth-child(2) > span:nth-child(2)",
   row2Plase3: "main > section div:nth-child(2) > span:nth-child(3)",
@@ -20,17 +9,6 @@ module.exports = {
   row2Plase8: "main > section div:nth-child(2) > span:nth-child(8)",
   row2Plase9: "main > section div:nth-child(2) > span:nth-child(9)",
   row2Plase10: "main > section div:nth-child(2) > span:nth-child(10)",
-
-  row3Plase1: "main > section div:nth-child(3) > span:nth-child(1)",
-  row3Plase2: "main > section div:nth-child(3) > span:nth-child(2)",
-  row3Plase3: "main > section div:nth-child(3) > span:nth-child(3)",
-  row3Plase4: "main > section div:nth-child(3) > span:nth-child(4)",
-  row3Plase5: "main > section div:nth-child(3) > span:nth-child(5)",
-  row3Plase6: "main > section div:nth-child(3) > span:nth-child(6)",
-  row3Plase7: "main > section div:nth-child(3) > span:nth-child(7)",
-  row3Plase8: "main > section div:nth-child(3) > span:nth-child(8)",
-  row3Plase9: "main > section div:nth-child(3) > span:nth-child(9)",
-  row3Plase10: "main > section div:nth-child(3) > span:nth-child(10)",
 
   row1PlaseVip:
     "section  div:nth-child(1) > span.buying-scheme__chair.buying-scheme__chair_vip",
